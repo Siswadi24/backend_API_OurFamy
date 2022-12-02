f8d566e4fa7333e4a975db4e354f71706705b494
Siswadi24 <danaperdanaputra32@gmail.com>
2022-12-02 15:36:47 +0700

    push configurasi deploy ke-2

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,15 +1,24 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB_URI: "mysql://root:k3iZdljKcsmLp2Pfuh1H@containers-us-west-50.railway.app:5506/railway",
-  PORT: 3000,
-  DB: "db_ourfamy",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB_URI: "mysql://root:k3iZdljKcsmLp2Pfuh1H@containers-us-west-50.railway.app:5506/railway",
+//   PORT: 3000,
+//   DB: "db_ourfamy",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+
+//connect to remote db
+const db = new Sequelize(config.DB_URI, {
+  define : {
+    timestamps: false
   }
-};
+});
+
+module.exports = db;
