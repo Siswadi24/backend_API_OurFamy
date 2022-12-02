79b5455b79b3303487eee04a329e0e3e7270ff18
Siswadi24 <danaperdanaputra32@gmail.com>
2022-12-02 16:19:39 +0700

    configurasi deploy ke-4

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,36 +1,36 @@
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "",
-//   DB_URI: "mysql://root:k3iZdljKcsmLp2Pfuh1H@containers-us-west-50.railway.app:5506/railway",
-//   PORT: 3000,
-//   DB: "db_ourfamy",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
-const dotenv = require('dotenv');
-const Sequelize = require('sequelize');
-
-dotenv.config();
-
-const {
-  DB_NAME,
-  DB_PASSWORD,
-  DB_USER,
-  DB_URI
-} = process.env;
-
-//connect to remote db
-const db = new Sequelize(config.DB_URI, {
-  define : {
-    timestamps: false
+module.exports = {
+  HOST: "containers-us-west-50.railway.app",
+  USER: "root",
+  PASSWORD: "k3iZdljKcsmLp2Pfuh1H",
+  DB_URI: "mysql://root:k3iZdljKcsmLp2Pfuh1H@containers-us-west-50.railway.app:5506/railway",
+  PORT: 5506,
+  DB: "railway",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
-});
+};
 
-module.exports = db;
+// const dotenv = require('dotenv');
+// const Sequelize = require('sequelize');
+
+// dotenv.config();
+
+// const {
+//   DB_NAME,
+//   DB_PASSWORD,
+//   DB_USER,
+//   DB_URI
+// } = process.env;
+
+// //connect to remote db
+// const db = new Sequelize(config.DB_URI, {
+//   define : {
+//     timestamps: false
+//   }
+// });
+
+// module.exports = db;
